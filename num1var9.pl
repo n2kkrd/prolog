6c8094687b846b7a57ea307503516325314ca083
n2kkrd <65614635+n2kkrd@users.noreply.github.com>
2020-06-10 11:57:28 +0300

    Update num1var9.pl

--- num1var9.pl
+++ num1var9.pl
@@ -8,7 +8,7 @@ solve(P,Cur_P,Max_P,Cur_C,Max_count):-P1 is P+1, solve(P1,Cur_P,Max_P,Cur_C,Max_
 
 
 split(P,A,_,_,Number,Number):- A is P div 3,!.
-split(P,A,B,_,Number,Count):- B is (P-A) div 2, A1 is A + 1,B1 is A1,C1 is P-A1,
+split(P,A,B,_,Number,Count):- B is (P-A) div 2, A1 is A + 1,B1 is A1,C1 is P-A1-B1,
 	(check_exist(A1,B1,C1),check_rightTriangle(A1,B1,C1)->Count1 is Count+1,
 	split(P,A1,B1,C1,Number,Count1),!; split(P,A1,B1,C1,Number,Count),!).
 split(P,A,B,_,Number,Count):- B1 is B+1, C1 is P - A -B1,
