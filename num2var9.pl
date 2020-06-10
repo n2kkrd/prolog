67005a2cad44db4e72a7c1efae8642f227524402
n2kkrd <65614635+n2kkrd@users.noreply.github.com>
2020-06-10 11:49:19 +0300

    Update num2var9.pl

--- num2var9.pl
+++ num2var9.pl
@@ -16,3 +16,24 @@ split(P,A,B,_,Number,Count):- B1 is B+1, C1 is P - A -B1,
 
 check_exist(A,B,C):- C < A + B.
 check_rightTriangle(A,B,C):- C2 is C*C,A2 is A*A,B2 is B*B, C2 is A2 + B2.
+
+
+
+
+
+
+
+r_list(A,N):-r_list(A,N,0,[]).
+r_list(A,N,N,A):-!.
+r_list(A,N,I,B):-I1 is I+1, read(X), append(B,[X],B1),r_list(A,N,I1,B1).
+
+naib([],_):-!.
+naib([H|T],M):-H>M, write(H),write(" "),naib(T,M).
+naib([_|T],M):-naib(T,M).
+
+
+sr_ar([],S,K,[H1|T1]):- Z is S/K,write(Z),write(" - Среднеее арифметическое"),nl, naib([H1|T1],Z),!.
+sr_ar([H|T],S,K,[H1|T1]):- S1 is S+H, K1 is K+1, sr_ar(T,S1,K1,[H1|T1]).
+
+
+ind2:-read(N),r_list(A,N),sr_ar(A,0,0,A).
